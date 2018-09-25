4b2ddd9a45c63fd98aff6df44c9a57808e9c379a
MizunagiKB <mizukb@LIVE.JP>
2018-09-25 21:25:20 +0900

    Window size changed

--- ui_MainWindow.py
+++ ui_MainWindow.py
@@ -11,7 +11,7 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(1024, 800)
+        MainWindow.resize(1024, 960)
         MainWindow.setAcceptDrops(False)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
